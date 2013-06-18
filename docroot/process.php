c311267db9533b0c0438ee27b387785820ba2cb5
David Burns <david@lullabot.com>
2013-06-18 09:30:58 -0400

    adjusting theme to fit our needs, colors, labels, text, format

--- docroot/process.php
+++ docroot/process.php
@@ -1,5 +1,5 @@
 <?php
-//Retrieve form data. 
+//Retrieve form data.
 //GET - user submitted data using AJAX
 //POST - in case user does not support javascript, we'll use POST instead
 $name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
@@ -12,21 +12,21 @@ if ($_POST) $post=1;
 
 //Simple server side validation for POST data, of course, you should validate the email
 if (!$name) $errors[count($errors)] = 'Please enter your name.';
-if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
-if (!$comment) $errors[count($errors)] = 'Please enter your comment.'; 
+if (!$email) $errors[count($errors)] = 'Please enter your email.';
+if (!$comment) $errors[count($errors)] = 'Please enter your comment.';
 
 //if the errors array is empty, send the mail
 if (!$errors) {
 
 	//recipient
-	$to = 'Your Name <youremailhere@googlemail.com>';	
+	$to = 'DRIPQUIP <david@dripquip.com>';
 	//sender
 	$from = $name . ' <' . $email . '>';
-	
+
 	//subject and the html message
-	$subject = 'Comment from ' . $name;	
+	$subject = 'Comment from ' . $name;
 	$message = '
-	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 	<head></head>
@@ -42,17 +42,17 @@ if (!$errors) {
 
 	//send the mail
 	$result = sendmail($to, $subject, $message, $from);
-	
+
 	//if POST was used, display the message straight away
 	if ($_POST) {
-		if ($result) echo 'Thank you! We have received your message.';
+		if ($result) echo 'Thank you! We have received your message. Someone will contact you soon.';
 		else echo 'Sorry, unexpected error. Please try again later';
-		
-	//else if GET was used, return the boolean value so that 
+
+	//else if GET was used, return the boolean value so that
 	//ajax script can react accordingly
 	//1 means success, 0 means failed
 	} else {
-		echo $result;	
+		echo $result;
 	}
 
 //if the errors array has values
@@ -69,9 +69,9 @@ function sendmail($to, $subject, $message, $from) {
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 	$headers .= 'From: ' . $from . "\r\n";
-	
+
 	$result = mail($to,$subject,$message,$headers);
-	
+
 	if ($result) return 1;
 	else return 0;
 }
